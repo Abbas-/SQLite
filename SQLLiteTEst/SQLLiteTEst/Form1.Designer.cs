fb19dee4b563a958485a54cb3446d0cd87dd32e4
Abbas ÇÖLGEÇEN <ce.abbas.colgecen@hotmail.com>
2019-04-10 01:29:18 +0300

    SQLite Commit

---
+++ SQLLiteTEst/SQLLiteTEst/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SQLLiteTEst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_PersonelName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_PersonelSoyadi = new System.Windows.Forms.TextBox();
+            this.btn_PersonelInsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Personel Adı";
+            // 
+            // txt_PersonelName
+            // 
+            this.txt_PersonelName.Location = new System.Drawing.Point(15, 25);
+            this.txt_PersonelName.Name = "txt_PersonelName";
+            this.txt_PersonelName.Size = new System.Drawing.Size(121, 20);
+            this.txt_PersonelName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Personel SoyAdı";
+            // 
+            // txt_PersonelSoyadi
+            // 
+            this.txt_PersonelSoyadi.Location = new System.Drawing.Point(147, 25);
+            this.txt_PersonelSoyadi.Name = "txt_PersonelSoyadi";
+            this.txt_PersonelSoyadi.Size = new System.Drawing.Size(121, 20);
+            this.txt_PersonelSoyadi.TabIndex = 2;
+            // 
+            // btn_PersonelInsert
+            // 
+            this.btn_PersonelInsert.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_PersonelInsert.Location = new System.Drawing.Point(274, 9);
+            this.btn_PersonelInsert.Name = "btn_PersonelInsert";
+            this.btn_PersonelInsert.Size = new System.Drawing.Size(284, 37);
+            this.btn_PersonelInsert.TabIndex = 3;
+            this.btn_PersonelInsert.Text = "Personel Kayıt";
+            this.btn_PersonelInsert.UseVisualStyleBackColor = true;
+            this.btn_PersonelInsert.Click += new System.EventHandler(this.btn_PersonelInsert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 190);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 253);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_PersonelInsert);
+            this.Controls.Add(this.txt_PersonelSoyadi);
+            this.Controls.Add(this.txt_PersonelName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_PersonelName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_PersonelSoyadi;
+        private System.Windows.Forms.Button btn_PersonelInsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SQLLiteTEst/SQLLiteTEst/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+namespace SQLLiteTEst
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection _Con;
+        SQLiteDataAdapter _adp;
+        SQLiteCommand _cmd;
+        DataSet _DS;
+        string SQLQUERY = @"";
+        public Form1()
+        {
+            InitializeComponent();
+            _DS = new DataSet();
+            _Con = new SQLiteConnection("Data Source=_TestDb.sqlite;Version=3;");
+            _cmd = new SQLiteCommand();
+
+        }
+        void GetPersonList()
+        {
+            _Con.Open();
+            _adp = new SQLiteDataAdapter("Select PersonID,PersonFirstName,PersonLastName,datetime('now','localtime') as TarihSaat from PersonTBL", _Con);
+            _DS.Clear();//
+            _adp.Fill(_DS, "PersonTBL");
+            _Con.Close();
+            dataGridView1.DataSource = _DS;
+            dataGridView1.DataMember = "PersonTBL";
+
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("_TestDb.sqllite"))//File(Dosyanın Olup olmadığını Kontrol Ediyor.
+            {
+                SQLiteConnection.CreateFile("_TestDb.sqllite");//Eğer Yoksa Oluşturyor
+                SQLQUERY = @"Create Table PersonTBL(
+                                  PersonID INTEGER PRIMARY KEY  AUTOINCREMENT ,
+                                  PersonFirstName TEXT NOT NULL,
+                                  PersonLastName TEXT NOT NULL
+                                  );";//Table Oluşturuyor Yenide
+                _Con.Open();
+                _cmd = new SQLiteCommand(SQLQUERY, _Con);//Gerekli işlemler
+                _cmd.ExecuteNonQuery();
+                _Con.Close();
+            }
+            else
+            {
+            }
+        }
+
+        private void btn_PersonelInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                SQLQUERY = @"Insert Into PersonTBL(PersonFirstName,PersonLastName) Values (@PersonFirstName,@PersonLastName)";
+                _Con.Open();
+                _cmd.Connection = _Con;
+                _cmd.CommandText = SQLQUERY;
+                _cmd.CommandType = CommandType.Text;
+                _cmd.Parameters.Add(new SQLiteParameter("@PersonFirstName", txt_PersonelName.Text));
+                _cmd.Parameters.Add(new SQLiteParameter("@PersonLastName", txt_PersonelSoyadi.Text));
+                int TaskResult = _cmd.ExecuteNonQuery();
+
+
+                _Con.Close();
+                if (TaskResult >= 1)
+                {
+                    GetPersonList();
+                }
+                else
+                {
+                    MessageBox.Show("fdsfs");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Data.ToString());
+            }
+        }
+    }
+}
